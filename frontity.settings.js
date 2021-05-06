e4a688630bb02a96fdf203f39e8b15ab38502db8
Jester Lopez <Jesmark1999@gmail.com>
2021-05-06 15:28:06 -0600

    url

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,34 +1,27 @@
 const settings = {
-  "name": "tactic-center",
-  "state": {
-    "frontity": {
-      "url": "http://tactic-center.test",
-      "title": "Tactic Center",
-      "description": "Agencia de publicidad"
-    }
+  name: "tactic-center",
+  state: {
+    frontity: {
+      url: "http://admin-tactic.tactic-center.com/",
+      title: "Tactic Center",
+      description: "Agencia de publicidad",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "tactic-theme",
+      name: "tactic-theme",
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": "http://tactic-center.test/wp-json/",
-          "postTypes": [
-            {
-              type: "servicios",
-              endpoint: "servicios",
-              archive: "/servicios"
-            }
-        ]
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api: "http://admin-tactic.tactic-center.com/wp-json/",
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
